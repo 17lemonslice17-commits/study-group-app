11029926c248e30f543dccaf8d6a12ffa1dee690
hjw <17lemonslice17@gmail.com>
2026-09-02 19:16:12 +0900

    chore: 배포용 DB 연결 방식 및 API URL 환경변수화

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
 });
 
-// 로그인 후 저장해둔 토큰을 모든 요청에 자동으로 붙여줌
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
